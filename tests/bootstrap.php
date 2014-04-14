12ff973c7ee8e7fa197809f9d87a7d9df4411ff7
Jason Roman <jroman@nabancard.com>
2014-04-14 04:15:45 -0400

    testing

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Flot unit tests bootstrap file
+ * 
+ * @author Jason Roman <j@jayroman.com>
+ */
+$loader = require __DIR__ . "/../vendor/autoload.php";
+$loader->addPsr4('JasonRoman\\', __DIR__.'/JasonRoman');
