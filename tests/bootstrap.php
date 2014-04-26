559548721fed345e3739bc90f6ea9e5fb3db0773
Jason Roman <jroman@nabancard.com>
2014-04-26 12:45:03 -0400

    adding travis-ci file back and fixed bootstrap vendor directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,8 +3,14 @@
 /**
  * Flot unit tests bootstrap file
  * 
- * This assumes there is an autoload.php file in the directory above this package
+ * This assumes there is an autoload.php file in the vendor directory above this package
  * 
  * @author Jason Roman <j@jayroman.com>
  */
-$loader = require __DIR__.'/../../../autoload.php';
+$file = __DIR__.'/../vendor/autoload.php';
+
+if (!file_exists($file)) {
+    throw new RuntimeException('Install dependencies to run test suite. "php composer.phar install --dev"');
+}
+
+require_once $file;
